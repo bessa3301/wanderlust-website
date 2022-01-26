bbe4e79572a6d73fc650b21cb7791e3c395184bc
kvass33 <bessa.3301@gmail.com>
2022-01-25 23:15:22 -0300

    created layout component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <title>Laravel</title>
-</head>
-
-<body>
-
+<x-layout>
     <div class="p-10 bg-wsblue-100">
         <h1> TailwindCSS v3.0 </h1>
     </div>
-</body>
-
-</html>
+</x-layout>
