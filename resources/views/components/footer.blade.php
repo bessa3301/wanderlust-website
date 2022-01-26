d4aeb23001e147ecfcda0cc99db19fb69543d73f
kvass33 <bessa.3301@gmail.com>
2022-01-25 23:25:52 -0300

    updated footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+    <footer>
+        <span> Address: { address } </span>
+        <span> Mon - Fri | 9am to 5pm BRT </span>
+        <span> {contact us} </span>
+        <span> CNPJ: {cnpj} </span>
+        <span> &copy 2022 - Wanderlust NGO </span>
+        <span> socials: </span>
+        <ul>
+            <li> insta </li>
+            <li> yt </li>
+            <li> etc </li>
+        </ul>
+    </footer>
 </div>
