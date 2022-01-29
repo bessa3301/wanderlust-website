1bde2ee12370d2ac574bb4a977354e0ac684af89
kvass33 <bessa.3301@gmail.com>
2022-01-29 19:09:22 -0300

    preparing for lang implm

--- app/Enum/LangsEnum.php
+++ app/Enum/LangsEnum.php
@@ -4,7 +4,7 @@ namespace App\Enum;
 
 class LangsEnum
 {
-    const BRAZILIAN_PORTUGUESE = "pt-br";
-    const UNITED_STATES_ENGLISH = "en-us";
+    const BRAZILIAN_PORTUGUESE = "pt";
+    const UNITED_STATES_ENGLISH = "en";
     const EXISTING_LANGUAGES = [self::BRAZILIAN_PORTUGUESE, self::UNITED_STATES_ENGLISH];
 }
