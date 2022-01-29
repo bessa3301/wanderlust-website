cb11fdac55e6482b83478e57119b672b10c3e139
kvass33 <bessa.3301@gmail.com>
2022-01-29 18:52:58 -0300

    included more items to lang enum

--- app/Enum/LangsEnum.php
+++ app/Enum/LangsEnum.php
@@ -6,4 +6,5 @@ class LangsEnum
 {
     const BRAZILIAN_PORTUGUESE = "pt-br";
     const UNITED_STATES_ENGLISH = "en-us";
+    const EXISTING_LANGUAGES = [self::BRAZILIAN_PORTUGUESE, self::UNITED_STATES_ENGLISH];
 }
