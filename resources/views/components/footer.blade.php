842411c97833975e33577deee3b8487be433dc3d
kvass33 <bessa.3301@gmail.com>
2022-02-06 17:56:31 -0300

    edit footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,23 @@
 <div>
-    <footer>
-        <span> Address: { address } </span>
-        <span> Mon - Fri | 9am to 5pm BRT </span>
-        <span> {contact us} </span>
-        <span> CNPJ: {cnpj} </span>
-        <span> &copy 2022 - Wanderlust NGO </span>
-        <span> socials: </span>
-        <ul>
-            <li> insta </li>
-            <li> yt </li>
-            <li> etc </li>
-        </ul>
+    <footer class="flex flex-col">
+        <div>
+            <span> Address: { address } </span>
+        </div>
+        <div>
+            <span> Mon - Fri | 9am to 5pm BRT </span>
+            <span> {contact us} </span>
+        </div>
+        <div>
+            <span> CNPJ: {cnpj} </span>
+            <span> &copy 2022 - Wanderlust NGO </span>
+        </div>
+        <div>
+            <span> socials: </span>
+            <ul>
+                <li> insta </li>
+                <li> yt </li>
+                <li> etc </li>
+            </ul>
+        </div>
     </footer>
 </div>
