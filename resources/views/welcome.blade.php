8153d5cc45a5f2b7fddd2f9d286f9917d11325b7
kvass33 <bessa.3301@gmail.com>
2022-02-06 18:32:14 -0300

    added first section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,20 @@
 <x-layout>
-    <div class="p-10 bg-wsorange-100">
-        <h1> {{ __('home.title1') }} </h1>
+    <div class="relative">
+        <img src="https://via.placeholder.com/1080x1920">
+
+        <!-- text -->
+        <div class="absolute bottom-36">
+            <h2 class="text-2xl p-2 text-center">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aliquam illum voluptatum velit.
+            </h2>
+        </div>
+
+        <!-- cta -->
+        <div class="absolute bottom-10 left-1/3">
+            <a class="text-center bg-wsorange-100 text-wsblue-100 py-4 p-3 rounded-lg" href="">
+                APPLY NOW!
+            </a>
+        </div>
     </div>
 </x-layout>
