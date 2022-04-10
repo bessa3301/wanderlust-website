d11951ce1993183d22f8adc5b82751695baa569c
kvass33 <bessa.3301@gmail.com>
2022-04-10 17:03:47 -0300

    5 | editing title content component

--- resources/views/livewire/title-and-content.blade.php
+++ resources/views/livewire/title-and-content.blade.php
@@ -1,3 +1,13 @@
-<div>
-    If your happiness depends on money, you will never be happy with yourself.
-</div>
+<section class="mx-3 my-4">
+    <div class="text-2xl flex justify-center py-3">
+        <h2> <b> {TITLE TITLE TITLE TITLE} </b> </h2>
+    </div>
+    <article class="text-gray-800">
+        <p class="indent-8">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus libero vel nihil et harum a minima illo
+            recusandae excepturi eos saepe dolorem atque quaerat asperiores soluta amet ullam fugit enim optio ratione,
+            error facere unde sit assumenda. Harum, ipsa assumenda libero maxime odit exercitationem. Assumenda
+            laudantium temporibus sequi minus eum.
+        </p>
+    </article>
+</section>
