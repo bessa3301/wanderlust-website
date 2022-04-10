b20bf7b603b4cf9ebaa4c08917b78aca3b3bf9c7
kvass33 <bessa.3301@gmail.com>
2022-04-10 20:57:19 -0300

    7 | populating new page

--- resources/views/get-involved.blade.php
+++ resources/views/get-involved.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
-    <div>
-        get involved i guess
-    </div>
+
+    @livewire('title-and-video',[
+    'title' => 'GET INVOLVED ... text',
+    'gifPath' => 'https://giphy.com/embed/1L9AwWrIhDFbBX9AZl',
+    ])
+
+    @livewire('title-and-content', [
+    'title' => 'More about getting involved',
+    'content' =>
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt similique alias rem cum neque sapiente nesciunt!
+    Ipsum, cumque. Eveniet neque, consequatur quas earum praesentium quis minima numquam distinctio reprehenderit hic
+    cum tenetur rerum maxime, ratione vel tempore? Eaque similique nisi tenetur fuga excepturi, ullam, eos, omnis sed
+    amet deleniti exercitationem?'
+    ])
+
+    @livewire('title-and-content', [
+    'title' => 'some more stuff you guys want to talk about idk',
+    'content' =>
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt similique alias rem cum neque sapiente nesciunt!
+    vel tempore? Eaque similique nisi tenetur fuga excepturi, ullam, eos, omnis sedamet deleniti exercitationem?'
+    ])
+
 </x-layout>
