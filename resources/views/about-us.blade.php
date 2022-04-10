4667d8e63894132a9e3076b9cf9e02d71574a8a9
kvass33 <bessa.3301@gmail.com>
2022-04-10 17:11:38 -0300

    5 | including more prop usage

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,7 +1,16 @@
 <x-layout>
 
     @livewire('title-and-content', [
-    'title' => 'TITLE TITLE TITLE TITLE',
+    'title' => 'ABOUT US',
+    'content' =>
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt similique alias rem cum neque sapiente nesciunt!
+    Ipsum, cumque. Eveniet neque, consequatur quas earum praesentium quis minima numquam distinctio reprehenderit hic
+    cum tenetur rerum maxime, ratione vel tempore? Eaque similique nisi tenetur fuga excepturi, ullam, eos, omnis sed
+    amet deleniti exercitationem?'
+    ])
+
+    @livewire('title-and-content', [
+    'title' => 'Some more details',
     'content' =>
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt similique alias rem cum neque sapiente nesciunt!
     Ipsum, cumque. Eveniet neque, consequatur quas earum praesentium quis minima numquam distinctio reprehenderit hic
