e2b21b8b413fe29ecacf02d37d7aeb42b0d2dedd
kvass33 <bessa.3301@gmail.com>
2022-04-10 20:12:48 -0300

    8 | css basic styling

--- resources/views/terms-and-conditions.blade.php
+++ resources/views/terms-and-conditions.blade.php
@@ -1,7 +1,22 @@
 <x-layout>
-
     <div class="mx-auto p-3">
-        {{ $term }}
+        <style scoped>
+            h2 {
+                font-size: 2em;
+                line-height: 1.5em;
+            }
+
+            h3 {
+                font-size: 1.25em;
+                line-height: 1em;
+            }
+
+            div {
+                /* styles for div */
+            }
+
+        </style>
+        {!! html_entity_decode($term) !!}
     </div>
 
 </x-layout>
